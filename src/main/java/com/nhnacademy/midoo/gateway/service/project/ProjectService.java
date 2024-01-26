e9ac998bab80c6b85a4ca0e405691b952cc61c6a
= <ydh4745@gmail.com>
2024-01-26 21:42:02 +0900

    ProjectService.java - createProject, getProjectDetail method 추가 ProjectServiceImpl.java - createProject 구현, getProjectDetail 선언

--- src/main/java/com/nhnacademy/midoo/gateway/service/project/ProjectService.java
+++ src/main/java/com/nhnacademy/midoo/gateway/service/project/ProjectService.java
@@ -1,8 +1,14 @@
 package com.nhnacademy.midoo.gateway.service.project;
 
+import com.nhnacademy.midoo.gateway.domain.ProjectPostRequest;
+import com.nhnacademy.midoo.gateway.domain.task.entity.ProjectDetail;
 import com.nhnacademy.midoo.gateway.domain.task.entity.ProjectInfo;
 import java.util.List;
 
 public interface ProjectService {
     List<ProjectInfo> getProjectsByAccountId(String projectId);
+
+    ProjectDetail createProject(ProjectPostRequest projectPostRequest);
+
+    ProjectDetail getProjectDetail(Long projectId);
 }
--- src/main/java/com/nhnacademy/midoo/gateway/service/project/ProjectServiceImpl.java
+++ src/main/java/com/nhnacademy/midoo/gateway/service/project/ProjectServiceImpl.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.midoo.gateway.service.project;
 
 import com.nhnacademy.midoo.gateway.config.task.TaskApiServerProperties;
+import com.nhnacademy.midoo.gateway.domain.ProjectPostRequest;
+import com.nhnacademy.midoo.gateway.domain.task.entity.ProjectDetail;
 import com.nhnacademy.midoo.gateway.domain.task.entity.ProjectInfo;
 import java.util.List;
 import org.springframework.core.ParameterizedTypeReference;
@@ -38,5 +40,29 @@ public class ProjectServiceImpl implements ProjectService {
                 });
 
         return responseEntity.getBody();
+    }
+
+    @Override
+    public ProjectDetail createProject(ProjectPostRequest projectPostRequest) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+        String url = taskUrl + "/projects/register/";
+
+        HttpEntity<ProjectPostRequest> requestEntity = new HttpEntity<>(projectPostRequest, httpHeaders);
+        ResponseEntity<ProjectDetail> responseEntity = template.exchange(
+                url,
+                HttpMethod.POST,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                });
+
+        return responseEntity.getBody();
+
+    }
+
+    @Override
+    public ProjectDetail getProjectDetail(Long projectId) {
+        return null;
     }
 }
