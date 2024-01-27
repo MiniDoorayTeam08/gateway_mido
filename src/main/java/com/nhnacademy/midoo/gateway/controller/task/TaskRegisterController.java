d91563190cf446cc0f02c16ad2e0e004fa890fbc
= <ydh4745@gmail.com>
2024-01-27 20:20:49 +0900

    task 등록 성공

--- src/main/java/com/nhnacademy/midoo/gateway/controller/task/TaskRegisterController.java
+++ src/main/java/com/nhnacademy/midoo/gateway/controller/task/TaskRegisterController.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.midoo.gateway.controller.task;
 
+import com.nhnacademy.midoo.gateway.config.IdProperties;
 import com.nhnacademy.midoo.gateway.domain.TaskPostRequest;
 import com.nhnacademy.midoo.gateway.service.task.TaskService;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +14,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/tasks/register")
 public class TaskRegisterController {
     private final TaskService taskService;
-
-    public TaskRegisterController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    private final IdProperties idProperties;
 
     @GetMapping("/{projectId}")
     public String getTaskRegisterForm(@PathVariable("projectId") long projectId,
+                                      HttpServletRequest request,
                                       Model model) {
+        model.addAttribute("accountId", request.getSession().getAttribute(idProperties.getId()));
         model.addAttribute("projectId", projectId);
         model.addAttribute("milestones", taskService.getMilestoneByProjectId(projectId));
         model.addAttribute("tags", taskService.getTagsByProjectId(projectId));
@@ -33,6 +36,6 @@ public class TaskRegisterController {
     public String postTask(@ModelAttribute TaskPostRequest taskPostRequest) {
         taskService.postTask(taskPostRequest);
 
-        return "redirect:/";
+        return "redirect:/project/" + taskPostRequest.getProjectId();
     }
 }
